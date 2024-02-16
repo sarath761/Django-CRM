55c302ad210724bb058005f19ba7754b4baa4d50
sarath761 <snairsarath761@gmail.com>
2024-02-16 15:43:22 +0530

    registration_Finished

--- CRMapp/forms.py
+++ CRMapp/forms.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
-from .models import Record
+#from .models import Record
 
 
 
--- CRMapp/views.py
+++ CRMapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
-
+from .forms import SignUpForm
 
 def home(request):
     if request.method == 'POST':
@@ -27,8 +27,24 @@ def logout_user(request):
 
 
 def register_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+			# Authenticate and login
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "You Have Successfully Registered! Welcome!")
+            return redirect('home')
+    else:
+        form = SignUpForm()
+        return render(request, 'register.html', {'form':form})
+
+    return render(request, 'register.html', {'form':form})
+
     
     
-    return render(request,'register.html',{})
 
 
